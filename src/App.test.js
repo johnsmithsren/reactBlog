e598dc6cfc72a4c843e954360c61c1678d13acd5
renjm <jianmin.ren@jfjun.com>
2019-07-24 21:56:39 +0800

    引入ant

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Hello from "./components/hello";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  // ReactDOM.render(<App />, div);
+  // const div1 = document.createElement("div");
+  ReactDOM.render(<Hello />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/components/hello/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+// 定义一个 hello 组件
+class Hello extends Component {
+  render() {
+    // const info = this.props.info;
+    return <div> welcome to my blog</div>;
+  }
+}
+export default Hello;
